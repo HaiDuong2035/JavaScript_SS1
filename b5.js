e68a9b51ec69a0b8b8a1f532c596362739fe3195
HaiDuong2035 <dfg.hd.2035@gmail.com>
2026-01-28 22:06:11 +0700

    Update b5.js

--- b5.js
+++ b5.js
@@ -1,6 +1,7 @@
-let name = prompt("Nhap ten sach");
-let publish = +prompt("Nhap nawm xuat ban cua sach");
+let nameBook = prompt("Nhap ten sach");
+let publish = +prompt("Nhap nam xuat ban cua sach");
 let year = new Date().getFullYear();
-console.log("Ten sach: ", name);
+console.log("Ten sach: ", nameBook);
 console.log("Nam xuat ban", publish);
 console.log(`Tuoi cua sach ${year-publish} nam`);
+
